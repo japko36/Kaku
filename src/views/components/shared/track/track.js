167f1919f17ab30f40496db6c8648d0aef2bb46e
Paweł <36593870+japko36@users.noreply.github.com>
2018-03-25 21:26:47 +0200

    update track.js to enter Drag and Drop sorting
    update track.js to enter React Drag and Drop sorting songs in playlist. This is one of the modified three files. These three files are:
    Kaku-1.9.0/src/views/modules/Player.js ,
    Kaku-1.9.0/src/views/components/shared/tracks.js  and 
    Kaku-1.9.0/src/views/components/shared/track/track.js .
    In order for the introduced changes to work, one should install: react-dnd and react-dnd-html5-backend. You also need to create an ItemTypes.js file in Kaku-1.9.0/src/views/components/shared/track/  .

--- src/views/components/shared/track/track.js
+++ src/views/components/shared/track/track.js
@@ -12,10 +12,59 @@ import TrackSquare from './track-square';
 import TabManager from '../../../modules/TabManager';
 
 import BasePlaylist from 'kaku-core/models/playlist/BasePlaylist';
+import { DragSource, DropTarget } from 'react-dnd';
+import ItemTypes from './ItemTypes';
 
 const Remote = Electron.remote;
 const Menu = Remote.Menu;
 const MenuItem = Remote.MenuItem;
+
+const trackSource = {
+  beginDrag (props) {
+    return {
+      id: props.id,
+      index: props.index,
+    }
+  },
+}
+const trackTarget = {
+
+  hover (props, monitor, component) {
+    const dragIndex = monitor.getItem().index
+    const hoverIndex = props.index
+
+    if (dragIndex === hoverIndex) {
+      return
+    }
+
+    const hoverBoundingRect = ReactDOM.findDOMNode(component).getBoundingClientRect()
+    const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+    const clientOffset = monitor.getClientOffset()
+    const hoverClientY = clientOffset.y - hoverBoundingRect.top
+
+    if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+      return
+    }
+    if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+      return
+    }
+
+    Track.moveTrack(dragIndex, hoverIndex)
+
+    monitor.getItem().index = hoverIndex
+  },
+}
+function collectdrop (connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget(),
+  }
+}
+function collectdrag (connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
+  }
+}
 
 class Track extends Component {
   constructor(props) {
@@ -27,6 +76,7 @@ class Track extends Component {
 
     this._setPlayingTrack = this._setPlayingTrack.bind(this);
     this._createContextMenu = this._createContextMenu.bind(this);
+    Track.moveTrack = Track.moveTrack.bind(this);	  
   }
 
   componentDidMount() {
@@ -54,6 +104,65 @@ class Track extends Component {
       Player.addTracks([track]);
       Player.playNextTrack(0);
     }
+  }
+  static moveTrack (dragIndex, hoverIndex) {
+    let playlists = PlaylistManager.playlists;
+    let temp = [];
+    let dragTrack;
+
+    playlists.forEach((playlist) => {
+      BasePlaylist.prototype.swapTracks = function (tracks) {
+        var promise = new Promise((resolve) => {
+          if (this._tracks.length <= 0) {
+            return Promise.resolve();
+          } else {
+            temp = this._tracks.slice(0);
+
+            dragTrack = this.tracks[dragIndex];
+
+            let onetrackplaylist = [];
+            onetrackplaylist.push(dragTrack);
+
+            this.tracks.splice(dragIndex, 1);
+            this.tracks.splice(hoverIndex, 0, onetrackplaylist[0]);
+
+            this.emit('tracksUpdated');
+
+            if (PlaylistManager.activePlaylist.isSameWith(playlist)) {
+              Player.randomIndexes = Player.makeRandomIndexes(Player.tracks.length);
+              if (Player.randomIndexes.length === temp.length) {
+                let playingIndex = this.tracks.indexOf(Player.playingTrack);
+                if (playingIndex === dragIndex) {
+                  let noUpdate = true;
+                  Player.cleanupTracks(noUpdate);
+                  Player.addTracks(this.tracks);
+                  Player.playNextTrack(playingIndex, Player.playingTrackTime);
+                } else {
+                  let noUpdate = true;
+                  Player.cleanupTracks(noUpdate);
+                  Player.addTracks(this.tracks);
+                }
+              }
+            } else {
+              return;
+            }
+          }
+
+          resolve();
+        }
+
+        );
+        return promise;
+      }
+      if (PlaylistManager.activePlaylist.isSameWith(playlist)) {
+        playlist
+          .swapTracks()
+          .catch((error) => {
+            Notifier.alert(error);
+          });
+      }
+    }
+    )
   }
 
   _clickToShowContextMenu(track, event) {
@@ -91,84 +200,7 @@ class Track extends Component {
             });
         };
       })(playlist);
-
-   let clickTrackup = ((playlist) => {
-          return ()=> {
-		  
-              let onetrackplaylist = [];
-              onetrackplaylist.push(track);
-
-              let ix = this.props.index;
-              BasePlaylist.prototype.swapTracks = function(tracks) {
-                      if (this._tracks.length <= 0) {
-                        return Promise.resolve();
-                      }
-                      else {
-
-                        this._tracks.splice(ix,1);
-                        var swapedTracks = this._tracks.splice(ix-1,0,onetrackplaylist[0]);
-                        console.log('Swaped tracks - ', swapedTracks);
-
-                        this.emit('tracksUpdated');
-
-                        let noUpdate = true;
-                        Player.cleanupTracks(noUpdate);
-                        Player.addTracks(this._tracks);
-
-                        resolve();
-                        return Promise.all(promises).then(() => {
-                          this.emit('tracksUpdated');
-                        });
-                      }
-                    };
-
-              playlist
-                 .swapTracks()
-                 .catch((error) => {
-                    Notifier.alert(error);
-                  });
-            }
-        })(playlist);
-
-      let clickTrackdown = ((playlist) => {
-          return ()=> {
-
-              let onetrackplaylist = [];
-              onetrackplaylist.push(track);
-
-              let ix = this.props.index;
-              BasePlaylist.prototype.swapTracks = function(tracks) {
-                      if (this._tracks.length <= 0) {
-                        return Promise.resolve();
-                      }
-                      else {
-
-                        this._tracks.splice(ix,1);
-                        var swapedTracks = this._tracks.splice(ix+1,0,onetrackplaylist[0]);
-                        console.log('Swaped tracks - ', swapedTracks);
-
-                        this.emit('tracksUpdated');
-
-                        let noUpdate = true;
-                        Player.cleanupTracks(noUpdate);
-                        Player.addTracks(this._tracks);
-
-                        resolve();
-                        return Promise.all(promises).then(() => {
-                          this.emit('tracksUpdated');
-                        });
-                      }
-                    };
-
-              playlist
-                 .swapTracks()
-                 .catch((error) => {
-                    Notifier.alert(error);
-                  });
-            }
-        })(playlist);
-
-      
+    
       // TODO
       // add l10n support here
       let menuItemToAddTrack = new MenuItem({
@@ -180,21 +212,10 @@ class Track extends Component {
         label: `Remove from ${playlist.name}`,
         click: clickToRemoveTrack
       });
-
-        let menuItemTrackUp = new MenuItem({
-        label: `Move track up`,
-        click: clickTrackup
-      });
-      let menuItemTrackDown = new MenuItem({
-          label: `Move track down`,
-          click: clickTrackdown
-      });
       
       if (PlaylistManager.isDisplaying) {
         if (PlaylistManager.activePlaylist.isSameWith(playlist)) {
           menu.append(menuItemToRemoveTrack);
-          menu.append(menuItemTrackUp);
-          menu.append(menuItemTrackDown);
         }
         else {
           menu.insert(0, menuItemToAddTrack);
@@ -213,6 +234,11 @@ class Track extends Component {
   }
 
   render() {
+      const {
+      connectDragSource,
+      connectDropTarget,
+    } = this.props	  
+	  
     let mode = this.props.mode;
     let track = this.props.data;
     let trackClassName = ClassNames({
@@ -251,6 +277,7 @@ class Track extends Component {
     let trackUI;
     let trackProps = {
       track: track,
+      moveTrack: Track.moveTrack.bind(this, track),	    
       onClick: this._clickToPlay.bind(this, track),
       onContextMenu: this._clickToShowContextMenu.bind(this, track),
       iconClassName: iconClassName,
@@ -265,11 +292,16 @@ class Track extends Component {
       trackUI = <TrackList {...trackProps} />;
     }
 
-    return trackUI;
+    return connectDragSource(
+      connectDropTarget(<div>{trackUI}</div>)
+    )
   }
 }
 
 Track.propTypes = {
+  connectDragSource: PropTypes.func.isRequired,
+  connectDropTarget: PropTypes.func.isRequired,
+  isDragging: PropTypes.bool.isRequired,	    
   data: PropTypes.object.isRequired,
   mode: PropTypes.string,
   index: PropTypes.number
@@ -281,4 +313,5 @@ Track.defaultProps = {
   index: -1
 };
 
-module.exports = Track;
+const x = DropTarget(ItemTypes.TRACK, trackTarget, collectdrop)(Track)
+module.exports = DragSource(ItemTypes.TRACK, trackSource, collectdrag)(x);
